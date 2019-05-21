79ac6a56b9c5f38b541a8285a839ac5cd267848a
Shine <jsyuan.jian@gmail.com>
2019-05-22 03:12:54 +0800

    44 add DP and backtracking

--- 44_wildcar_matching.py
+++ 44_wildcar_matching.py
@@ -6,26 +6,73 @@ class Solution(object):
         :rtype: bool
         """
 
-        if not p:
-            return not s
+        s_len, p_len = len(s), len(p)
+        dp = [[False for _ in xrange(s_len+1)] for _ in xrange(p_len+1)]
+        dp[0][0] = True # s[:0]  p[:0] match
 
-        if p[0] == "*":
-            while 1 < len(p) and "*" == p[1]: p = p[1:]
+        def print_dp():
+            print '      ' + '  '.join(s)
+            for idx in xrange(p_len + 1):
+                if idx == 0 : print " ",
+                else: print p[idx-1],
+                print map(lambda x: 1 if x else 0, dp[idx])
+            print 
+
+        for i in xrange(1, p_len+1):
+            if p[i-1] != "*": break
+            dp[i][0] = True
+
+        for p_idx in xrange(1, p_len+1):
+            for s_idx in xrange(1, s_len+1):
+                if (p[p_idx-1] in ['?', s[s_idx-1]]) and dp[p_idx-1][s_idx-1]:
+                    dp[p_idx][s_idx] = True 
+                 
+                elif p[p_idx-1] == "*" and (dp[p_idx-1][s_idx-1] or dp[p_idx-1][s_idx]):
+                    for k in xrange(s_idx, s_len+1):
+                        dp[p_idx][k] = True
+                    continue
+        print_dp()
+        return dp[-1][-1]
+
+
+    # def isMatch(self, s, p):
+    #     """
+    #     :type s: str
+    #     :type p: str
+    #     :rtype: bool
+    #     """
+    #     self.s_len, self.p_len = len(s), len(p)
+    #     s_idx = p_idx = 0
+    #     stack = []
+    #     while s_idx < self.s_len:
+    #         if p_idx == self.p_len and not stack:
+    #             return False
+
+    #         elif p_idx < self.p_len and p[p_idx] in ['?', s[s_idx]]:
+    #             s_idx +=1
+    #             p_idx +=1
+    #         elif p_idx < self.p_len and p[p_idx] == "*":
+    #             while p_idx + 1 < self.p_len and "*" == p[p_idx+1]: 
+    #                 p_idx += 1
+    #             stack.append((s_idx+1, p_idx+1))
+    #             p_idx += 1
+    #         elif not stack:
+    #             return False
+    #         else:
+    #             s_idx, p_idx = stack[-1]
+    #             stack[-1] = (s_idx+1, p_idx)
             
-            return self.isMatch(s, p[1:]) or (bool(s) and  self.isMatch(s[1:], p))
-        elif not s:
-            return False
-        elif p[0] in ['?', s[0]]:
-            return self.isMatch(s[1:], p[1:])
-        else:
-            return False
+    #     return all(x == '*' for x in p[p_idx:])
+        
 
 if __name__ == "__main__":
-    print Solution().isMatch("acdcb", "a*c?b")
+    # print Solution().isMatch("a", "a*")
+    # print Solution().isMatch("aa", "*")
+    print Solution().isMatch("acdcba", "a*c?b")
     # print Solution().isMatch("adceb", "*a*b")
     # print Solution().isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b")
     # print Solution().isMatch("mississippi", "m??*ss*?i*pi")
     
-
+    # print Solution().isMatch("axccxxcxxdcxxxcb", "a*c*c*?b")
     # print Solution().isMatch("bbaaaabaaaaabbabbabbabbababaabababaabbabaaabbaababababbabaabbabbbbbbaaaaaabaabbbbbabbbbabbabababaaaaa",
     # "******aa*bbb*aa*a*bb*ab***bbba*a*babaab*b*aa*a****")
