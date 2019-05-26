0acdb8ff79e83d46c7550f5a8c3892dff28d5be3
Shine <jsyuan.jian@gmail.com>
2019-05-26 17:52:05 +0800

    56

---
+++ 56_merge_interval.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        idx = 0
+        while idx < len(intervals) - 1:
+            current, next_interval = intervals[idx], intervals[idx + 1]
+            if next_interval[0] - current[1] <= 0:
+                intervals[idx][1] = next_interval[1]
+                del intervals[idx + 1]
+            else:
+                idx += 1
+        
+        return intervals
+if __name__ == "__main__":
+    print Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]])
